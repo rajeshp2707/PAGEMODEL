3db3804cb20f7fbcf7a67f09fde0a03860135371
rajeshp2707 <162096605+rajeshp2707@users.noreply.github.com>
2024-03-14 22:44:49 +0530

    Update TestBase.java

--- src/main/java/com/crm/qa/base/TestBase.java
+++ src/main/java/com/crm/qa/base/TestBase.java
@@ -41,7 +41,7 @@ public class TestBase {
 		String browserName = prop.getProperty("browser");
 		
 		if(browserName.equals("chrome")){
-			System.setProperty("webdriver.chrome.driver", "C:/tools/selenium/chromedriver");	
+			System.setProperty("webdriver.chrome.driver", "C:/tools/selenium/");	
 			driver = new ChromeDriver(); 
 		}
 		else if(browserName.equals("FF")){
